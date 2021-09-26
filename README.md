918964be5100e7d3e4f5e8876ed118f2bd0200a1
Sayyed Viquar ahmed <sviquarahmed@gmail.com>
2021-09-26 15:17:14 +0530

    add
    Added something to the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # umberlla
+![Screenshot]("https://wallpaperaccess.com/full/452898.jpg")
 cli based tool for sms and phonce calling bombing 
---
+++ src/sms.py
@@ -0,0 +1,11 @@
+import time as t 
+import sys as s
+import phonenumbers
+
+def sendsms(number):
+    t.sleep(5)
+    print("[*] Vadalting the number ")
+    for g in range (0,4):
+        t.sleep(5)
+        print("[*] Searching Number information ")
+    
