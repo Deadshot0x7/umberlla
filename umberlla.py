c08f2a5d8da67cb7410053112104b533ebc063ea
Sayyed Viquar ahmed <sviquarahmed@gmail.com>
2021-09-26 15:41:09 +0530

    added
    added banner

---
+++ umberlla.py
@@ -0,0 +1,15 @@
+import time as t 
+import pyfiglet as p 
+import sys as s 
+import logging
+def printbanner():
+    banner= p.figlet_format("Umberlla",font="bulbhead")
+    print(banner)
+
+
+
+
+
+if __name__ == "__main__":
+   
+    printbanner()
